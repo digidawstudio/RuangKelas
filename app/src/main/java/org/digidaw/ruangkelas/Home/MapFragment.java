3484847a5ffadef124ca0e47964de6c2c796368b
adorufus <adorufus@gmail.com>
2017-11-16 13:12:15 +0700

    make change

--- app/src/main/java/org/digidaw/ruangkelas/Home/MapFragment.java
+++ app/src/main/java/org/digidaw/ruangkelas/Home/MapFragment.java
@@ -10,24 +10,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
 import org.digidaw.ruangkelas.R;
 
 /**
  * Created by STURMBANNFUEHRER on 29/10/2017.
  */
 
-public class MapFragment extends Fragment implements OnMapReadyCallback{
+public class MapFragment extends Fragment{
 
-    GoogleMap mGoogleMap;
-    MapView mMapView;
     View mView;
 
     @Override
@@ -40,24 +30,5 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mView = inflater.inflate(R.layout.fragment_map,container, false);
         return mView;
-    }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState){
-        super.onViewCreated(view, savedInstanceState);
-
-        mMapView = (MapView) mView.findViewById(R.id.mapView);
-        if(mMapView != null){
-            mMapView.onCreate(null);
-            mMapView.onResume();
-            mMapView.getMapAsync(this);
-        }
-    }
-
-    public void onMapReady(GoogleMap map){
-        MapsInitializer.initialize(getContext());
-        mGoogleMap = map;
-        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
 }
